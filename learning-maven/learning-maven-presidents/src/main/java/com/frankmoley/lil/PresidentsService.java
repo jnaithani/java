fd4fb0563f32996833e4cdb30807362a010b9fe1
Jayesh Naithani <jnaithani@gmail.com>
2021-01-17 17:28:40 +0000

    Building shaded uber jars

--- learning-maven/learning-maven-presidents/src/main/java/com/frankmoley/lil/PresidentsService.java
+++ learning-maven/learning-maven-presidents/src/main/java/com/frankmoley/lil/PresidentsService.java
@@ -54,5 +54,9 @@ public class PresidentsService {
         LOGGER.info("Presidents have been loaded");
     }
 
+    public static void main(String[] args) {
+        PresidentsService service = new PresidentsService();
+        service.getPresidents().forEach(System.out::println);
+    }
 
 }
