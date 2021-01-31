68df77317befe4a8198320d9599d9d1219e13a07
Jayesh Naithani <jnaithani@gmail.com>
2021-01-31 10:16:34 -0600

    Adding room web app from LinkedIn Learning course SBET

---
+++ learning-maven/room-web-app/src/main/java/org/naithani/roomwebapp/RoomWebAppApplication.java
@@ -0,0 +1,13 @@
+package org.naithani.roomwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomWebAppApplication.class, args);
+	}
+
+}
---
+++ learning-maven/room-web-app/src/main/java/org/naithani/roomwebapp/controllers/StaffRestController.java
@@ -0,0 +1,25 @@
+package org.naithani.roomwebapp.controllers;
+
+import java.util.List;
+
+import org.naithani.roomwebapp.models.StaffMember;
+import org.naithani.roomwebapp.service.StaffService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/staff")
+public class StaffRestController {
+
+    private final StaffService staffService;
+
+    public StaffRestController(StaffService staffService) {
+        this.staffService = staffService;
+    }
+
+    @GetMapping
+    public List<StaffMember> getAllStaff(){
+        return staffService.getAllStaff();
+    }
+}
