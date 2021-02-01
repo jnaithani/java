56fc7d983adbd4655ee3b5c166bddbe5a806ed5c
Jayesh Naithani <jnaithani@gmail.com>
2021-01-31 20:16:30 -0600

    Creating a Spring Data repository

--- learning-maven/room-web-app/src/main/java/org/naithani/roomwebapp/service/RoomService.java
+++ learning-maven/room-web-app/src/main/java/org/naithani/roomwebapp/service/RoomService.java
@@ -1,23 +1,21 @@
 package org.naithani.roomwebapp.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.naithani.roomwebapp.data.RoomRepository;
 import org.naithani.roomwebapp.models.Room;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RoomService {
 
-    private static final List<Room> rooms = new ArrayList<>();
+    private final RoomRepository roomRepository;
 
-    static{
-        for(int i=0;i<10;i++){
-            rooms.add(new Room(i, "Room " + i, "R"+i, "Q"));
-        }
+    public RoomService(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
     }
 
     public List<Room> getAllRooms(){
-        return rooms;
+        return roomRepository.findAll();
     }
 }
